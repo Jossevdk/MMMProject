f6cd15e3676a4a8ed18db333c151d21feeedfe17
Jossevdk <josse.vdk@gmail.com>
2024-04-19 17:35:52 +0200

    everything working, nice pulse

--- UCHIE_ata.py
+++ UCHIE_ata.py
@@ -4,7 +4,6 @@ import copy
 import matplotlib.animation as animation 
 import copy
 import pandas as pd
-import scipy as sp
 
 
 
@@ -19,7 +18,7 @@ class Source:
         
     #This will call the function depending on which type of source you have    
     def J(self, t):
-        return self.J0*np.exp(-(t-self.tc)**2/(2*self.sigma**2))#*np.cos(2e10*t)
+        return self.J0*np.exp(-(t-self.tc)**2/(2*self.sigma**2))
 
 
 
@@ -38,11 +37,9 @@ class UCHIE:
 
         X = np.zeros((2*Nx+2, Ny)) # the first Nx+1 rows are the Ey fields, and the others the Bz fields
         Ex = np.zeros((Nx+1, Ny+1))
-        
 
         A_D = np.diag(-1 * np.ones(Nx+1), 0) + np.diag(np.ones(Nx), 1)
         A_D[Nx, 0] = 1
-        #print(A_D.shape)
 
         A_I = np.diag(1 * np.ones(Nx+1), 0) + np.diag(np.ones(Nx), 1)
         A_I[Nx, 0] = 1
@@ -56,7 +53,7 @@ class UCHIE:
         # M1[0, 0] = 1
         # M1[Nx + 1, :] = np.zeros( 2*Nx+2)
         # M1[Nx + 1, Nx + 1] = 1
-        M1_inv = sp.linalg.inv(M1)
+        M1_inv = np.linalg.inv(M1)
 
         M2_top = np.hstack((-1/dx*A_D, 1/dt*A_I))
         M2_bot = np.hstack((eps/dt*A_I, -1/(mu*dx)*A_D))
@@ -71,25 +68,19 @@ class UCHIE:
 
 
     ### Update ###
-    def explicit(self, n, Ex, Bz, dy, dt, eps, mu):
-
-        # S = np.zeros((Nx-1, Ny-1))
-        # S[ int(source.x/dx), int(source.y/dy)] = source.J(n*dt)*dt
-        Ex[1:-1, 1:-1] = Ex[1:-1, 1:-1] + dt/(eps*dy*mu) * (Bz[1:-1,1: ] - Bz[1:-1,:-1 ]) 
+    def explicit(self, Ex, Bz, dy, dt, eps, mu):
+        Ex[1:-1, 1:-1] = Ex[1:-1, 1:-1] + dt/(eps*dy*mu) * (Bz[1:-1, 1:] - Bz[1:-1, :-1])
 
 
         return Ex
 
 
     def implicit(self, n, X, Ex, dx, dy, dt, Nx, Ny, M1_inv, M2, source):
-        
         Y = Ex[:-1, 1:] + Ex[1:, 1:] - Ex[:-1, :-1] - Ex[1:, :-1]
 
         S = np.zeros((2*Nx+2, Ny))
-        S[Nx+ int(source.x/dx), int(source.y/dy)] = source.J(n*dt)
-    
-        #X = M1_inv@M2@X + M1_inv@np.vstack((Y, S))/dy #+ M1_inv@S
-        X = M1_inv@M2@X + M1_inv@np.vstack((Y, np.zeros((Nx+2, Ny))))/dy +M1_inv@S
+        S[Nx + int(source.x/dx), int(source.y/dy)] = source.J(n*dt)
+        X = M1_inv@M2@X + M1_inv@np.vstack((Y, np.zeros((Nx+2, Ny))))/dy + M1_inv@S
         
 
         return X
@@ -101,8 +92,8 @@ class UCHIE:
         data = []
 
         for n in range(0, Nt):
-            X= self.implicit(n, X ,Ex, dx, dy, dt, Nx, Ny, M1_inv, M2, source)
-            Ex = self.explicit(n, Ex, X[Nx+1:,:], dy, dt, eps, mu)
+            X = self.implicit(n, X, Ex, dx, dy, dt, Nx, Ny, M1_inv, M2, source)
+            Ex = self.explicit(Ex, X[Nx+1:,:], dy, dt, eps, mu)
             data_time.append(dt*n)
             data.append(copy.deepcopy((X[Nx+1:,:].T)))
             
@@ -144,17 +135,17 @@ class UCHIE:
 
 eps0 = 8.854 * 10**(-12)
 mu0 = 4*np.pi * 10**(-7)
+c0 = 299792458
 
-dx = 0.01 # m
+dx = 0.005 # m
 dy = 0.01 # ms
-c = 299792458 # m/s
+
 Sy = 0.8 # !Courant number, for stability this should be smaller than 1
-dt = Sy*dy/c
+dt = Sy*dy/c0
 
 Nx = 300
 Ny = 300
 Nt = 500
-
 # dx = 1 # m
 # dy = 2 # ms
 # c = 299792458 # m/s
@@ -166,8 +157,8 @@ Nt = 500
 # Nt = 100
 
 
-xs = 0.8
-ys = 0.8
+xs = Nx*dx/2
+ys = Ny*dy/2
 
 tc = dt*Nt/3
 print(tc)
